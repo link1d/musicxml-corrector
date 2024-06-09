ae2b7a0c348a9130177f51dd43c7b47407d6bb51
Sebastian Martinez <sebastianmartinez@MacBook-Pro-de-Sebastian-2.local>
2024-06-09 18:55:43 -0500

    Display Sheet

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hola mundo!");
+import { OpenSheetMusicDisplay } from "opensheetmusicdisplay";
+
+// importar los datos del MusicXML
+import data from "./data.js";
+
+// Mostrar los datos en la consola del browser
+console.log(data);
+
+// crear un elemento de contenedor de HTMl <div>
+const container = document.createElement("div");
+
+//Agregar el elemento a la pagina
+document.body.appendChild(container);
+
+// Crea una instancia de OpenSheetMusicDisplay y adjuntala
+// al contendedor que creamos en el anterior paso
+const osmd = new OpenSheetMusicDisplay(container);
+
+// This is an asynchronous IIFE
+(async () => {
+  // carga el xml a OpenSheetMusicDisplay
+  await osmd.load(data);
+  // Renderiza la partitura
+  osmd.render();
+})();
