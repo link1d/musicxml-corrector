b56290e3336a43ec7b750bbaf157de7991bd5e31
Sebastian Martinez <sebastianmartinez@MacBook-Pro-de-Sebastian-2.local>
2024-06-10 08:43:25 -0500

    Red Two Voices Trombones

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,8 @@ const osmd = new OpenSheetMusicDisplay(container);
 
   window["osmd"] = osmd;
 
+// TROMPETAS
+  // Esta es la manera de que lea las trompetas y que luego tome la primera y segunda voz
   const trompetas = osmd.Sheet.Instruments[2].Voices[0].VoiceEntries;
 
  const voice1 = trompetas.map(function (x){return x.Notes[3]});
@@ -55,7 +57,46 @@ const osmd = new OpenSheetMusicDisplay(container);
  console.log (voice1);
  console.log (voice2);
 
+
+ //TROMBONES
+ // Esta es la manera de que lea los trombones y que luego tome la primera y segunda voz
+ const trombones = osmd.Sheet.Instruments[3].Voices[0].VoiceEntries;
+
+ const voicetr1 = trombones.map(function (x){return x.Notes[3]});
+
+ const voicetr2 = trombones.map(function (x){return x.Notes[2]});
+
+ // Rule: the primary and secondary voices cannot form a major 3rd.
+  voicetr1.forEach(function (note1, i) {
+    const note2 = voicetr2[i];
  
+    if (!note1 || !note2) {
+      return;
+    }
+    // Array entries can be rests.
+    if (note1.isRest() || note2.isRest()) {
+      return;
+    }
+    const distance = Math.abs(note1.halfTone - note2.halfTone);
+    if (distance === 4) {
+      note1.NoteheadColor = "#FF0000";
+      note2.NoteheadColor = "#FF0000";
+    }
+  });
+
+  //Pruebas a ver qué pasa
+
+  const voices = [
+    trombones.map(entry => entry.Notes[3]),
+    trombones.map(entry => entry.Notes[2]),
+    trombones.map(entry => entry.Notes[1]),
+    trombones.map(entry => entry.Notes[0])
+  ];
+
+ //SAXOS
+ 
+ 
+  
 
 })();
 
